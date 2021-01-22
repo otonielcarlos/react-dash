b7a82ef47407dfcffbcdb16653a8be0ed5a6f9bd
Otoniel Carlos <otonielcarlosf@gmail.com>
2021-01-22 17:51:52 -0600

    retire contraseña

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const cors = require('cors');
 app.use(cors());
 
 const sql = mysql.createConnection({
-    host : '173.231.198.187',
+    host : '+++++',
     user: 'edulinkstore_DB',
-    password: '.-.bdi-2020.-.',
-    database: 'edulinkstore_DB'
+    password:'+++++',
+    database: '+++++',
+
 });
 
 sql.connect();
@@ -32,7 +33,7 @@ app.get('/', (req, res) => {
 app.post('/:sku', (req, res) => {
     let skuQuery = `SELECT p.product_id, p.sku, p.model, p.quantity, pd.name, c.name AS "Categoria" FROM product AS p JOIN product_description AS pd ON p.product_id = pd.product_id JOIN product_to_category AS pc ON pc.product_id = p.product_id JOIN category_description AS c ON c.category_id = pc.category_id WHERE p.sku = "${req.params.sku}"`;
     sql.query(skuQuery, (err, results) => {
-        if (err) res.send('No existe el sku')
+        if (err) throw err;
         res.send(results);
     });
 
